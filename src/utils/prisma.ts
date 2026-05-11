e22f3ba0cc98e300e6c42af50afddf5e85315991
zalayetha <zalayetha@gmail.com>
2026-05-11 20:24:15 +0700

    fix: delete db.dev and add gitignore for dev.db

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const adapter = new PrismaBetterSqlite3({
-  url: "file:./prisma/db.dev",
+  url: "file:./prisma/dev.db",
 });
 
 export const prisma = new PrismaClient({
